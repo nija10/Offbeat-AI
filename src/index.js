de9a9be415298c2889e0b0e08c974b9ab45df5af
nija10 <nijakharangar@gmail.com>
2025-03-06 22:55:11 +0530

    Integrated AI

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-function generateText(event){
-  event.preventDefault();
+function displayText(response){
 
   new Typewriter('#text', {
-  strings: "otters hold hands while sleeping...",
+  strings: response.data.answer,
   autoStart: true,
   delay:20,
   cursor:""
 });
+}
+
+
+function generateText(event){
+  event.preventDefault();
+
+  let instructionsInput=document.querySelector("#user-instructions")
+
+  let apiKey="2a6oe4748bb5fftba3d905acff9c17d3";
+  let context = "You are a witty and funny AI with knowledge about almost everything. Ensure the information is accurate. Share a fun fact about the given topic with a humorous twist. After the fact, insert a <br> element, then sign the poem with 'SheCodes AI' inside a <strong> element on a new line. Keep the fact concise and follow user instructions carefully.";
+  let prompt=`User instructions are: Give a fun fact about ${instructionsInput.value}`;
+  let apiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayText);
 
 }
 
